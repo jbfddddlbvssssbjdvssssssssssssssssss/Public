d721872ea9fe8f0df08dd4808554a561bb76bacd
jbfddddlbvssssbjdvssssssssssssssssss <85943419+jbfddddlbvssssbjdvssssssssssssssssss@users.noreply.github.com>
2021-07-11 18:38:04 +0300

    Added Task 2

---
+++ main.py
@@ -0,0 +1,281 @@
+import tkinter as tk
+import math
+
+
+root = tk.Tk()
+root.title('Task2.Calculator')
+root.configure(bg='#808080')
+#root.resizable(width=False, height=False)
+
+
+ent_field = tk.Entry(root, bg='#D3D3D3', fg='#000000', font=('Arial', 35),
+                     borderwidth=5, justify="right")
+ent_field.grid(row=0, columnspan=10, padx=5, pady=5,
+               sticky='n'+'s'+'e'+'w')
+ent_field.insert(0, '0')
+
+FONT = ('Arial', 20, 'bold')
+
+
+class SC_Calculator():
+    def __init__(self):
+        self.current = ''
+        self.inp_value = True
+        self.result = False
+
+    def Entry(self, value):
+        ent_field.delete(0, 'end')
+        ent_field.insert(0, value)
+
+    def Enter_Num(self, num):
+        self.result = False
+        firstnum = ent_field.get()
+        secondnum = str(num)
+        if self.inp_value:
+            self.current = secondnum
+            self.inp_value = False
+        else:
+            self.current = firstnum+secondnum
+        self.Entry(self.current)
+
+    def Standard_Ops(self, val):
+        temp_str = ent_field.get()
+        try:
+            if val == '=':
+                ans = str(eval(temp_str))
+                self.result = True
+                self.Entry(ans)
+            else:
+                self.Entry(temp_str+val)
+            self.inp_value = False
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Clear_Entry(self):
+        self.result = False
+        self.current = "0"
+        self.Entry(0)
+        self.inp_value = True
+
+    def SQ_Root(self):
+        try:
+            self.current = math.sqrt(float(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+
+    def Deg(self):
+        try:
+            self.result = False
+            self.current = math.degrees(float(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Rad(self):
+        try:
+            self.result = False
+            self.current = math.radians(float(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+
+
+    def Sin(self):
+        try:
+            self.result = False
+            self.current = math.sin(math.radians(float(ent_field.get())))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Cos(self):
+        try:
+            self.result = False
+            self.current = math.cos(math.radians(float(ent_field.get())))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Tan(self):
+        try:
+            self.result = False
+            self.current = math.tan(math.radians(float(ent_field.get())))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+
+
+
+    def Ln(self):
+        try:
+            self.result = False
+            self.current = math.log(float(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Log_10(self):
+        try:
+            self.result = False
+            self.current = math.log10(float(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+
+    def One_div_x(self):
+        try:
+            self.result = False
+            self.current = 1/(int(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Abs(self):
+        try:
+            self.result = False
+            self.current = abs(float(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+    def Binary(self):
+        try:
+            self.result = False
+            self.current = bin(int(ent_field.get()))
+            self.Entry(self.current)
+        except ValueError:
+            self.Entry('Error')
+        except SyntaxError:
+            self.Entry('Error')
+
+
+numberpad = "789456123"
+i = 0
+button = []
+for j in range(2, 5):
+    for k in range(3):
+        button.append(tk.Button(root, text=numberpad[i], font=FONT,
+                                fg="#000000", width=6, height=2,
+                                highlightbackground='#CCCCCC', highlightthickness=2))
+        button[i].grid(row=j, column=k, sticky='n' +
+                       's'+'e' + 'w', padx=3, pady=3)
+        button[i]["command"] = lambda x=numberpad[i]: sc_app.Enter_Num(x)
+        i += 1
+
+
+btn_CE = tk.Button(root, text='CE', command=lambda: sc_app.Clear_Entry(),
+                   font=FONT, height=2, fg="#000000",
+                   highlightbackground='#ADD8E6', highlightthickness=2)
+btn_CE.grid(row=1, column=0, columnspan=2,
+            sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_sqr = tk.Button(root, text='\u221A', command=lambda: sc_app.SQ_Root(),
+                    font=FONT, width=6, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_sqr.grid(row=1, column=2, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_0 = tk.Button(root, text='0', command=lambda: sc_app.Enter_Num('0'),
+                  font=FONT, width=6, height=2, fg="#000000",
+                  highlightbackground='#ADD8E6', highlightthickness=2)
+btn_0.grid(row=5, column=0, columnspan=2,
+           sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_point = tk.Button(root, text='.', command=lambda: sc_app.Standard_Ops('.'),
+                      font=FONT, width=6, height=2, fg="#000000",
+                      highlightbackground='#ADD8E6', highlightthickness=2)
+btn_point.grid(row=5, column=2, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_equal = tk.Button(root, text='=', command=lambda: sc_app.Standard_Ops('='),
+                      font=FONT, width=6, height=2, fg="#990000",
+                      highlightbackground='#ADD8E6', highlightthickness=2)
+btn_equal.grid(row=5, column=3, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_add = tk.Button(root, text='+', command=lambda: sc_app.Standard_Ops('+'),
+                    font=FONT, width=6, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_add.grid(row=1, column=3, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_sub = tk.Button(root, text='-', command=lambda: sc_app.Standard_Ops('-'),
+                    font=FONT, width=6, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_sub.grid(row=2, column=3, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_mul = tk.Button(root, text='*', command=lambda: sc_app.Standard_Ops('*'),
+                    font=FONT, width=6, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_mul.grid(row=3, column=3, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_div = tk.Button(root, text='/', command=lambda: sc_app.Standard_Ops('/'),
+                    font=FONT, width=6, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_div.grid(row=4, column=3, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+
+
+btn_sin = tk.Button(root, text='sin', command=lambda: sc_app.Sin(),
+                    font=FONT, width=5, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_sin.grid(row=1, column=4, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_cos = tk.Button(root, text='cos', command=lambda: sc_app.Cos(),
+                    font=FONT, width=5, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_cos.grid(row=2, column=4, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_tan = tk.Button(root, text='tan', command=lambda: sc_app.Tan(),
+                    font=FONT, width=5, height=2, fg="#990000",
+                    highlightbackground='#ADD8E6', highlightthickness=2)
+btn_tan.grid(row=3, column=4, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+
+
+btn_ln = tk.Button(root, text='ln', command=lambda: sc_app.Ln(),
+                   font=FONT, width=5, height=2, fg="#990000",
+                   highlightbackground='#ADD8E6', highlightthickness=2)
+btn_ln.grid(row=4, column=4, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+
+
+btn_1div_by_x = tk.Button(root, text='%', command=lambda: sc_app.One_div_x(),
+                          font=FONT, width=5, height=2, fg="#990000",
+                          highlightbackground='#ADD8E6', highlightthickness=2)
+btn_1div_by_x.grid(row=5, column=4, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+btn_binary = tk.Button(root, text='binary', command=lambda: sc_app.Binary(),
+                          font=FONT, width=5, height=2, fg="#990000",
+                          highlightbackground='#ADD8E6', highlightthickness=2)
+btn_binary.grid(row=1, column=5, rowspan=5, sticky='n'+'s'+'e'+'w', padx=3, pady=3)
+
+
+if __name__ == '__main__':
+
+    sc_app = SC_Calculator()
+
+    root.mainloop()
