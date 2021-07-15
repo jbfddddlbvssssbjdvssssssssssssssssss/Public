eebbfd273bf807737a3a05979d6445a9f025d7ab
jbfddddlbvssssbjdvssssssssssssssssss <85943419+jbfddddlbvssssbjdvssssssssssssssssss@users.noreply.github.com>
2021-07-15 16:10:27 +0300

    Updated task1

--- task1.py
+++ task1.py
@@ -1,57 +1,21 @@
+import re
 
-import os
-str = ""
+str = input("\nInput the string with numbers and words: ")
+word = ''.join([i for i in str if not i.isdigit()])
+nums = re.findall(r'\d+', str)
+nums = [int(i) for i in nums]
 
-words = []
-numbers = []
-numbers_raised = []
-numbers_max = 0
+print("\nYour string without numbers:", word)
+print("\nYour string without words:", nums)
 
-#Inputting certain string with words and numbers.
+WithLarge = ' '.join(word[0].upper() + word[1:-1] + word[-1:].upper() for word in word.split())
+print("\nUpdated string with uppercase letters:", WithLarge)
 
-str = input("Input your string: ")
+if not nums:
+    print("You inputed only words:(")
+else:
+    nums.remove(max(nums))
+    numberIndex = [nums[i]**i for i in range(0,len(nums))]
+    print("Array with numbers and indexes:", numberIndex)
 
-#Loop is checking whether string is in digits. Splitting and adding numbers to certain arrays(numbers) or (words). 
-
-for w in str.split():
-    if w.isdigit():
-        numbers.append(int(w))
-    else:
-        words.append(w)
-str = " ".join(words)
-
-#Output of the string with splitted words.
-
-print("\nOnly words:\n   ", str)
-
-#Loop is capitalising the first and the last letter of every word.
-
-for i in range(len(words)):
-    words[i] = words[i][0].upper() + words[i][1:-1] + words[i][-1].upper()
-str = " ".join(words)
-
-#Output of the updated string with uppercase letters.
-
-print("\nUpdated string with uppercase letters:\n   ", str)
-
-#Finding the max number with the help of average function max.
-
-#numbers_max = max(numbers)
-print("\nJust numbers:", numbers)
-print("The max number:", numbers_max)
-
-#Loop is comparing every number in the string to the max. If not adds it to array(numbers_raised), outputs its value, index and idk raised to its index(pidnesennia do stepenia). 
-
-print("Raised to index:")
-for i in range(len(numbers)):
-    if numbers[i] != numbers_max:
-        print("   ", numbers[i], "**", i, "=", numbers[i] ** i)
-        numbers_raised.append(numbers[i] ** i)
-    else:
-        print("   ", numbers[i], "is max(without changes).")
-
-#Output of the numbers raised to the index(kinda pidnesenni do stepenia).
-
-print("\nResult:", numbers_raised)
-
-os.system("pause")
+print("\n")
